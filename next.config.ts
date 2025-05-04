42b478e243941e561062155e558e889506f7057f
Ran Byron <ranbena@gmail.com>
2025-05-04 19:30:30 +0200

    Fixed config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
-  assetPath: process.env.PAGES_BASE_PATH,
+  assetPrefix: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
