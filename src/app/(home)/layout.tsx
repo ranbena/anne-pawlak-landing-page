a2d7f85ab4d211d1439145e9c795b10045dbc59d
Ran Byron <ranbena@gmail.com>
2025-05-28 15:33:10 +0200

    Updated homepage title

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,8 +4,9 @@ import type { Metadata } from 'next';
 import styles from './layout.module.css';
 
 export const metadata: Metadata = {
-  title: 'Anne Pawlak',
-  description: 'Verwandle deine Online-Präsenz in ein Online Business',
+  title: {
+    absolute: 'Anne Pawlak',
+  },
 };
 
 export default function HomepageLayout({
