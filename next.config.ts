861f6e7e85e9356bbca171dd14d6de72e3aac6a8
Ran Byron <ranbena@gmail.com>
2025-05-31 18:51:32 +0200

    Removed github-pages related config
    It is now pointing to annepawlak.de

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+  // basePath: process.env.PAGES_BASE_PATH,
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'], // Configure `pageExtensions` to include markdown and MDX files
 };
 
